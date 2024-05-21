e94bb3f449c85087dbab8aaa2be62f61857eabf7
tanscoder <149710255+tanscoder@users.noreply.github.com>
2024-05-21 22:20:05 +0530

    Create test.js
    testing java script basically

---
+++ test.js
@@ -0,0 +1,21 @@
+but1=document.getElementById("but1");
+output=document.getElementById("output");
+n=document.getElementById("input1");
+
+let out="hi there";
+
+output.textcontent
+
+but1.onclick=function(){ 
+output.textContent(n)
+
+if (n="hello"){ 
+
+output.textcontent("System says"+out.valueOf)
+
+}
+else{ 
+   console.log("value of N is not 10")
+   console.log("value of n int what i expected")
+}}
+
